ec035f28b6e8a53eed5042d270f0541596e31f8f
Kolopep <83094485+Kolopep@users.noreply.github.com>
2021-08-09 14:12:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -70,8 +70,7 @@ const subPickEmbed = new DiscordJS.MessageEmbed()
         let message = `Joined At: ${msg.member.joinedAt} | User: ${msg.member.user.username}${msg.member.user.tag} | Content: ${msg.content} | `;
         if(msg.member.joinedAt > new Date(2021, 7, 8, 17, 38) && msg.member.joinedAt < new Date(2021, 7, 8, 17, 43))
         {
-            message+=`Joined At: ${msg.member.joinedAt} | User: ${msg.member.user.username}${msg.member.user.tag} | Content: ${msg.content} | DELETE`;
-            console.log(message);
+            console.log(`Joined At: ${msg.member.joinedAt} | User: ${msg.member.user.username}${msg.member.user.tag} | Content: ${msg.content} | DELETE`);
             msg.delete();
         }
     });
