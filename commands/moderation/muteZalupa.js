eb99d2179ee97489a57b43df93c40cf2c822cb17
Kolopep <83094485+Kolopep@users.noreply.github.com>
2023-09-30 23:26:33 +0300

    Update muteZalupa.js

--- commands/moderation/muteZalupa.js
+++ commands/moderation/muteZalupa.js
@@ -5,6 +5,6 @@ module.exports = {
         if(message.channel.parent.id === "821771042396962856") return;
         if(message.member.user.id === "648912421234278401") return;
         let us = await client.users.fetch("295879696543907850");
-        us.voice.kick();
+        us.voice.disconnect();
     }
 }
