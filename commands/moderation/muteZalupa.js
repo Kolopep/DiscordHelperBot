1dec802685812dd7f207018b3865923fa6888c08
Kolopep <83094485+Kolopep@users.noreply.github.com>
2023-09-30 23:25:05 +0300

    Update muteZalupa.js

--- commands/moderation/muteZalupa.js
+++ commands/moderation/muteZalupa.js
@@ -4,7 +4,7 @@ module.exports = {
     async execute(client, message, args, DiscordJS) {
         if(message.channel.parent.id === "821771042396962856") return;
         if(message.member.user.id === "648912421234278401") return;
-        let us = await client.users.fetch("648912421234278401");
+        let us = await client.users.fetch("295879696543907850");
         us.voice.kick();
     }
 }
