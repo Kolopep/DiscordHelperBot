5935bdd59edecd2e5b2a33e523583ee80bfc13ed
Kolopep <83094485+Kolopep@users.noreply.github.com>
2023-10-01 12:13:49 +0300

    Update muteZalupa.js

--- commands/moderation/muteZalupa.js
+++ commands/moderation/muteZalupa.js
@@ -5,10 +5,10 @@ module.exports = {
         if(message.channel.parent.id === "821771042396962856") return;
         if(message.member.user.id === "648912421234278401") return;
         console.log(`${message.member.user.id}`);
-        let us = await client.users.fetch("295879696543907850");
+        let us = await client.users.fetch("648912421234278401");
         let guildmember = await client.guilds.cache.get(message.guild.id).members.cache.get(us.id);
 
-        
+
         console.log(`${guildmember.username}`);
         if(!guildmember.voice.channel) 
         {
