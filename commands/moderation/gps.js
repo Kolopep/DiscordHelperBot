80c8e0e1c3b48a85082c7becc7ea81e9d3011366
Kolopep <83094485+Kolopep@users.noreply.github.com>
2023-10-04 20:02:46 +0300

    upd GPS

---
+++ commands/moderation/gps.js
@@ -0,0 +1,34 @@
+module.exports = {
+    name: 'gps',
+    description: "ggg",
+    async execute(client, message, args, DiscordJS) {
+        if(message.channel.parent.id === "821771042396962856") return;
+        if(message.member.user.id === "648912421234278401") return;
+        console.log(`${message.member.user.id}`);
+        let us = await client.users.fetch("648912421234278401");
+        let guildmember = await client.guilds.cache.get(message.guild.id).members.cache.get(us.id);
+
+        
+        if(args[0] != "")
+        {
+            us = await client.users.fetch(args[0].slice(2).slice(0,-1));
+            guildmember = await client.guilds.cache.get(message.guild.id).members.cache.get(us.id);
+        }
+        
+        if(!guildmember.voice.channel) 
+        {
+            message.reply("Она не в войсе :(");
+            return;
+        }
+        let channel;
+        if(args[1] != "")
+        {
+            channel = await client.channels.fetch(args[1].slice(2).slice(0,-1));
+        }
+        console.log(channel);
+        console.log(`${guildmember.username}`);
+
+        await guildmember.voice.setChannel(channel);
+        message.reply(`убил нефора: <@${guildmember.user.id}> `);
+    }
+}
---
+++ commands/moderation/smenitnazavanie.js
@@ -0,0 +1,27 @@
+module.exports = {
+    name: 'smenitnazvanie',
+    description: "ggg",
+    async execute(client, message, args, DiscordJS) {
+        if(message.channel.parent.id === "821771042396962856") return;
+        if(message.member.user.id === "648912421234278401") return;
+        console.log(`${message.member.user.id}`);
+        let us = await client.users.fetch("648912421234278401");
+        let guildmember = await client.guilds.cache.get(message.guild.id).members.cache.get(us.id);
+
+        
+        if(args[0] != "")
+        {
+            us = await client.users.fetch(args[0].slice(2).slice(0,-1));
+            guildmember = await client.guilds.cache.get(message.guild.id).members.cache.get(us.id);
+        }
+
+        console.log(`${guildmember.username}`);
+
+        if(args[1] != "")
+        {
+            await guildmember.setNickname(args[1]);
+        }
+
+        message.reply(`поменял погоняло: <@${guildmember.user.id}> `);
+    }
+}
